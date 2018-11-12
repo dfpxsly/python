76e2e557e8a0a8912187699b2e3a0829d4d4fe8f
SLY <sly19901121@163.com>
2018-11-12 22:30:52 +0800

    py api

--- TestAPI/python_repos.py
+++ TestAPI/python_repos.py
@@ -6,7 +6,23 @@ print('status code: ', req.status_code)
 
 rsp_dict = req.json()
 
-print(rsp_dict.keys())
+#print(rsp_dict.keys())
 
-for idx, column_head in enumerate(rsp_dict['items']):
-	print(idx, column_head)
+print("total repos:", rsp_dict['total_count'])
+
+repo_dicts = rsp_dict['items']
+print("respos returned:", len(repo_dicts))
+
+'''
+repo_dict = repo_dicts[0]
+print("\nkeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+	print(key)
+'''
+
+for repo_dict in repo_dicts:
+	print('Name:', repo_dict['name'])
+	print('Owner:', repo_dict['owner']['login'])
+	print('Stars:', repo_dict['stargazers_count'])
+	print('Repository:', repo_dict['html_url'])
+	print('Description:', repo_dict['description'])
